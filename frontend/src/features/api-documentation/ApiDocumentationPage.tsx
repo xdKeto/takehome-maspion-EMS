b87f6217f7290b4cb931885a61e806d17a908994
Yestoya L Minggus <yestoya.minggus@gmail.com>
2026-09-21 00:29:52 +0700

    auth, login, routers, and api setup

--- frontend/src/features/api-documentation/ApiDocumentationPage.tsx
+++
@@ -1,7 +0,0 @@
-export function ApiDocumentationPage() {
-  return (
-    <section>
-      <h1 className="text-3xl">API Documentation</h1>
-    </section>
-  )
-}
--- frontend/src/features/employees/EmployeePage.tsx
+++
@@ -1,7 +0,0 @@
-export function EmployeePage() {
-  return (
-    <section>
-      <h1 className="text-3xl">Employee Page</h1>
-    </section>
-  )
-}
