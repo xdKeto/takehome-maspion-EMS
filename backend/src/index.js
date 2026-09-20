09d3d961b8d67ec27ba789e1184c43879a841f06
Yestoya L Minggus <yestoya.minggus@gmail.com>
2026-09-20 16:45:08 +0700

    prepare backend deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 import app from "./app"
 
-const PORT = process.env.PORT || 2000
+// const PORT = process.env.PORT || 2000
 
-app.listen(PORT, () => {
-  console.log("API running, port:", PORT)
-})
+// app.listen(PORT, () => {
+//   console.log("API running, port:", PORT)
+// })
 
 export default app
